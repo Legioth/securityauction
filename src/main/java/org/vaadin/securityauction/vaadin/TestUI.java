19e772d7fd6d9cae8e67a21875191680d2aecb4d
Leif Åstrand <leif@vaadin.com>
2013-09-13 09:35:14 +0300

    Add initial Vaadin setup

---
+++ src/main/java/org/vaadin/securityauction/vaadin/TestUI.java
@@ -0,0 +1,14 @@
+package org.vaadin.securityauction.vaadin;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+public class TestUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setContent(new Label("Here"));
+    }
+
+}
