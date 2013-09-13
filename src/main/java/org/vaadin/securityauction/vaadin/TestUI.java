374a99d49293a0491ed1a6aaf467b6423974c227
Kim L <kim@vaadin.com>
2013-09-13 10:19:25 +0300

    CDI stub working

--- src/main/java/org/vaadin/securityauction/vaadin/TestUI.java
+++ src/main/java/org/vaadin/securityauction/vaadin/TestUI.java
@@ -1,9 +1,11 @@
 package org.vaadin.securityauction.vaadin;
 
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
+@CDIUI
 public class TestUI extends UI {
 
     @Override
