7fe1c43d972318d11b6646fa82434b8d0e8c6508
Kim L <kim@vaadin.com>
2013-09-17 15:52:39 +0300

    Implementing JPA persistence

--- src/main/java/org/vaadin/securityauction/server/AuctionServiceImpl.java
+++ src/main/java/org/vaadin/securityauction/server/AuctionServiceImpl.java
@@ -1,8 +1,20 @@
 package org.vaadin.securityauction.server;
 
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.servlet.annotation.WebServlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.vaadin.securityauction.shared.AuctionItem;
 import org.vaadin.securityauction.shared.AuctionService;
 import org.vaadin.securityauction.shared.User;
@@ -13,17 +25,34 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/SecurityAuction/AuctionService" })
 public class AuctionServiceImpl extends RemoteServiceServlet implements
         AuctionService {
+    
+    @PersistenceUnit
+    private EntityManagerFactory factory;
 
-    @Override
     public User authenticate(String username, String password) {
-        if ("admin".equals(username) && "admin".equals(password)) {
-            return new User(0, "admin");
-        } else if ("user".equals(username) && "user".equals(password)) {
-            return new User(1, "user");
-        } else {
+        Subject currentUser = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username,
+                password);
+        EntityManager entityManager = null;
+        try {
+            currentUser.login(token);
+            entityManager = factory.createEntityManager();
+            Query query = entityManager
+                    .createQuery("SELECT u FROM User u WHERE username = :username");
+            query.setParameter("username", username);
+
+            // Execute query and return result
+            return (User) query.getSingleResult();
+        } catch (Exception e) {
+            Logger.getAnonymousLogger().log(Level.INFO, e.getMessage());
             return null;
+        } finally {
+            if(entityManager != null) {
+                entityManager.close();
+            }
         }
     }
 
